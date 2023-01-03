d6496624cf610a6e831d8e977ce9490eebc13652
Anjali Bathra <anjalibathra@utexas.edu>
2023-01-02 18:13:12 -0600

    Created Card component

--- src/lib/TransactionItem.svelte
+++ src/lib/TransactionItem.svelte
@@ -1,7 +1,48 @@
 <script>
+    import Card from "./ui/Card.svelte";
     export let item
 </script>
 
-<div>
-    {item.memo}
-</div>
+<Card>
+    <div class="date">
+        {item.date}
+    </div>
+    <div class="category">
+        {item.category}
+    </div>
+    <div class="text">
+        {item.memo}
+    </div>
+    <div class="amt">
+        {item.amount}
+    </div>
+    <div class="close">X</div>
+</Card>
+
+<style>
+    .date {
+        grid-area: date;
+        font-weight: 600;
+    }
+    .category {
+        grid-area: category;
+        justify-self: center;
+        text-transform: uppercase;
+    }
+
+    .text {
+        grid-area: text;
+    }
+
+    .amt {
+        grid-area: amt;
+    }
+
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 15px;
+        font-weight: 600;
+        cursor: pointer;
+    }
+</style>
---
+++ src/lib/ui/Card.svelte
@@ -0,0 +1,19 @@
+<div class="card">
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        width: 300px;
+        display: grid;
+        grid-template-areas: "date date date" 
+                             "category text amt";
+        align-content: center;
+        background-color: #fff;
+        color: #213547;
+        border-radius: 15px;
+        padding: 40px 50px;
+        margin: 20px 0;
+        position: relative;
+    }
+</style>
